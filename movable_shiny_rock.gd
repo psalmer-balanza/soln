bcf93d26a6e5517e1a7d8c068073c78f0e4d701b
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-06 03:40:24 +0800

    Cutscene testing and reformatting filenames

--- movable_shiny_rock.gd
+++ movable_shiny_rock.gd
@@ -1,20 +1,18 @@
 extends Node2D
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 var has_hidden_rock = false
 
-func _ready():
-	# Check the state of the rock on start
-	if State.rock_removed:
-		hide_rock()
-
 func _process(_delta):
 	if State.rock_removed and not has_hidden_rock:
-		print("Rock removed from the path.")
 		hide_rock()
+		has_hidden_rock = true
 
 func hide_rock():
-	# Annotated by ChatGPT OpenAI
-	$CollisionShape2D.disabled = true
+	animation_player.play("rock_disappear")
+	await animation_player.animation_finished
+	print("Rock disappeared")
+	$CollisionPolygon2D.disabled = true
 	$Sprite2D.visible = false
 	has_hidden_rock = true
 	
