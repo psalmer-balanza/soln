9ec4f0cb7b4cc006ed75c7c88923c48bc5ac528b
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-06 04:03:29 +0800

    Proper timiing for cutscenes

--- movable_shiny_rock.gd
+++ movable_shiny_rock.gd
@@ -1,18 +1,20 @@
 extends Node2D
 @onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
+
 var has_hidden_rock = false
+
 
 func _process(_delta):
 	if State.rock_removed and not has_hidden_rock:
 		hide_rock()
 		has_hidden_rock = true
 
-func hide_rock():
+func hide_rock() -> void:
 	animation_player.play("rock_disappear")
 	await animation_player.animation_finished
 	print("Rock disappeared")
 	$CollisionPolygon2D.disabled = true
-	$Sprite2D.visible = false
+	
 	has_hidden_rock = true
 	
