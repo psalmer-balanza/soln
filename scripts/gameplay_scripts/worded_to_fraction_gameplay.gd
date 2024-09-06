64f6dba8cfd8d8170c7837d0c224da3095b11a21
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-07 05:16:12 +0800

    basic worded fraction functinoality

---
+++ scripts/gameplay_scripts/worded_to_fraction_gameplay.gd
@@ -0,0 +1,68 @@
+extends Control
+
+# The user inputs all three fractions
+
+# FIRST FRACTION
+@onready var first_fraction_numerator: LineEdit = $FirstFraction/FirstFractionNumerator
+@onready var first_fraction_denominator: LineEdit = $FirstFraction/FirstFractionDenominator
+# SECOND FRACTION
+@onready var second_fraction_numerator: LineEdit = $SecondFraction/SecondFractionNumerator
+@onready var second_fraction_denominator: LineEdit = $SecondFraction/SecondFractionDenominator
+# ANSWER
+@onready var numerator_answer: LineEdit = $Answer/NumeratorAnswer
+@onready var denominator_answer: LineEdit = $Answer/DenominatorAnswer
+
+@onready var first_fraction_given: RichTextLabel = $FirstFraction/FractionOne
+@onready var second_fraction_given: RichTextLabel = $SecondFraction/FractionTwo
+@onready var display_answer: RichTextLabel = $DisplayAnswer/UserAnswer
+
+func _ready():
+	print("ready")
+	## Setting the text dynamically in the script
+	## For zen to read from db
+	#var fraction_text = "1 -- 4"
+	#first_fraction_given.bbcode_text = fraction_text
+	## For zen to read from db (second fraction) 
+	#fraction_text = "3 -- 4"
+	#second_fraction_given.bbcode_text = fraction_text
+
+# Checker of proper and improper fraction for addition
+func _on_submit_answer_button_down():
+	# Retrieve input values
+	var first_numerator = int(first_fraction_numerator.text)
+	var first_denominator = int(first_fraction_denominator.text)
+	var second_numerator = int(second_fraction_numerator.text)
+	var second_denominator = int(second_fraction_denominator.text)
+	var answer_numerator = int(numerator_answer.text)
+	var answer_denominator = int(denominator_answer.text)
+
+	# OS.alert('Please input your answer', 'Message Title')
+	fraction_addition_checker(first_numerator, first_denominator, second_numerator, second_denominator, answer_numerator, answer_denominator)
+	
+func fraction_addition_checker(first_numerator: int, first_denominator: int, second_numerator: int, second_denominator: int, answer_numerator: int, answer_denominator: int):
+	if first_denominator == second_denominator:
+		# If denominators are the same, just add the numerators
+		var added_numerator = first_numerator + second_numerator
+		
+		if added_numerator == answer_numerator and first_denominator == answer_denominator:
+			# If the user's answer is correct
+			display_answer.text = "Good job! \nCorrect answer!"
+		else:
+			# If the user's answer is incorrect
+			display_answer.text = "Try \nagain."
+	else:
+		# If denominators are different, find the least common denominator (LCD)
+		var lcd = GlobalFractionFunctions.get_lcd(first_denominator, second_denominator)
+		
+		# Adjust the numerators to the same denominator
+		var adjusted_first_numerator = first_numerator * (lcd / first_denominator)
+		var adjusted_second_numerator = second_numerator * (lcd / second_denominator)
+		
+		var added_adjusted_numerator = adjusted_first_numerator + adjusted_second_numerator
+		
+		if added_adjusted_numerator == answer_numerator and lcd == answer_denominator:
+			# If the user's answer is correct
+			display_answer.text = "Good job! \nCorrect answer!"
+		else:
+			# If the user's answer is incorrect
+			display_answer.text = "Try \nagain."
