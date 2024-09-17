5944d52d49440eea1eb5403875bbd10e4d04a2c3
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-18 00:16:48 +0800

    Changes to singleton files and cutscenes

--- movable_shiny_rock.gd
+++ movable_shiny_rock.gd
@@ -5,14 +5,16 @@ extends Node2D
 var has_hidden_rock = false
 
 func _process(_delta):
-	if State.rock_removed and not has_hidden_rock:
+	if DialogueState.rock_removed and not has_hidden_rock:
 		hide_rock()
 		has_hidden_rock = true
 
 func hide_rock() -> void:
+	$ExclamationMark.visible = false
 	animation_player.play("rock_disappear")
 	await animation_player.animation_finished
 	print("Rock disappeared")
+	
 	$CollisionPolygon2D.disabled = true
 	
 	has_hidden_rock = true
