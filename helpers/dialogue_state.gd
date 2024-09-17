bcbe0f28384b766b65c89bf5fd325a681af143a8
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-18 01:42:21 +0800

    Reverted the revert
    This reverts commit 5b46af4ef89d62e4da0f1cffbd428143b92e9d80.

---
+++ helpers/dialogue_state.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# For movable shiny rock cutscene
+var rock_removed: bool = false
+func remove_rock():
+	rock_removed = true
+	emit_signal("rock_removed")
+	print("Rock has been removed!")
+
+# For saisai moving rock cutscene
+var saisai_rock_moved: bool = false
+func saisai_move_rocks_cutscene():
+	saisai_rock_moved = true
+	emit_signal("saisai_rock_moved")
+	print("Saisai rock moved")
--- movable_shiny_rock.gd
+++ movable_shiny_rock.gd
@@ -5,11 +5,12 @@ extends Node2D
 var has_hidden_rock = false
 
 func _process(_delta):
-	if State.rock_removed and not has_hidden_rock:
+	if DialogueState.rock_removed and not has_hidden_rock:
 		hide_rock()
 		has_hidden_rock = true
 
 func hide_rock() -> void:
+	$ExclamationMark.visible = false
 	animation_player.play("rock_disappear")
 	await animation_player.animation_finished
 	print("Rock disappeared")
