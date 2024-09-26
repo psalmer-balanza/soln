d1446266c8c5e701f4fded45437279255ca5a2b8
2205867 <153579536+2205867@users.noreply.github.com>
2024-09-27 05:09:46 +0800

    lets just use placeholders!

--- scenes/characters_scenes/saisai.gd
+++ scenes/characters_scenes/saisai.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 func _process(_delta):
-	if PlayerState.saisai_quest_progress == 0:
+	if DialogueState.saisai_quest_progress == 0:
+		$Actionable/CollisionShape2D.disabled = true
 		$AnimatedSprite2D.visible = false
 		$CollisionPolygon2D.disabled = true
-	elif PlayerState.saisai_quest_progress == 1:
+	elif DialogueState.saisai_quest_progress == 1:
+		$Actionable/CollisionShape2D.disabled = false
 		$AnimatedSprite2D.visible = true
 		$CollisionPolygon2D.disabled = false
