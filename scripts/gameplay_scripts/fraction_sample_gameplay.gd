608d1bb051706975ebc81c8774673460fe131263
MattTicag <153583264+MattTicag@users.noreply.github.com>
2024-09-28 20:05:20 +0800

    fixed simple fraction gameplay

--- scripts/gameplay_scripts/fraction_sample_gameplay.gd
+++ scripts/gameplay_scripts/fraction_sample_gameplay.gd
@@ -23,11 +23,18 @@ func initiate_questions():
 
 var current_question_index = 0  # Track which question the player is on
 
-@onready var numerator_input: LineEdit = $Answer/NumeratorAnswer
-@onready var denominator_input: LineEdit = $Answer/DenominatorAnswer
-@onready var first_fraction_given: RichTextLabel = $FirstFraction/FractionOne
-@onready var second_fraction_given: RichTextLabel = $SecondFraction/FractionTwo
-@onready var display_answer: RichTextLabel = $DisplayAnswer/UserAnswer
+@onready var numerator_input: LineEdit = $addition/VBoxContainer/HBoxContainer/answer_fraction/fraction/numerator/NumeratorAnswer
+@onready var denominator_input: LineEdit = $addition/VBoxContainer/HBoxContainer/answer_fraction/fraction/denominator/DenominatorAnswer
+@onready var first_num_label:Label = $addition/VBoxContainer/HBoxContainer/first_fraction/fraction/numerator
+@onready var first_denum_label:Label = $addition/VBoxContainer/HBoxContainer/first_fraction/fraction/denominator
+@onready var second_num_label:Label = $addition/VBoxContainer/HBoxContainer/second_fraction/fraction/numerator
+@onready var second_denum_label:Label = $addition/VBoxContainer/HBoxContainer/second_fraction/fraction/denominator
+@onready var display_answer: Label = $addition/VBoxContainer/result
+
+var first_num:int
+var first_denum:int
+var second_num:int
+var second_denum:int
 
 func _ready():
 	# Start by displaying the first question
@@ -42,22 +49,16 @@ func display_current_question():
 	var second_fraction = current_question[1]
 	
 	# Set the text for the first and second fractions
-	first_fraction_given.bbcode_text = "%d -- %d" % [first_fraction[0], first_fraction[1]]
-	second_fraction_given.bbcode_text = "%d -- %d" % [second_fraction[0], second_fraction[1]]
-
-# Function called when the submit answer button is pressed
-func _on_submit_answer_button_down():
-	# Split the string using the right delimiter " -- "
-	var first_fraction_split = first_fraction_given.text.split(" -- ")
-	var second_fraction_split = second_fraction_given.text.split(" -- ")
+	first_num = first_fraction[0]
+	first_denum = first_fraction[1]
+	second_num = second_fraction[0]
+	second_denum = second_fraction[1]
 	
-	var first_numerator = int(first_fraction_split[0])
-	var first_denominator = int(first_fraction_split[1])
-	var second_numerator = int(second_fraction_split[0])
-	var second_denominator = int(second_fraction_split[1])
-
-	# Check the answer for the current fraction
-	fraction_addition_checker(first_numerator, first_denominator, second_numerator, second_denominator)
+	# display fractions
+	first_num_label.text = str(first_num)
+	first_denum_label.text = str(first_denum)
+	second_num_label.text = str(second_num)
+	second_denum_label.text = str(second_denum)
 
 # Function to check the fraction addition answer
 func fraction_addition_checker(first_numerator: int, first_denominator: int, second_numerator: int, second_denominator: int):
@@ -97,3 +98,9 @@ func next_question_or_finish():
 func return_to_world():
 	print("Returning")
 	get_tree().change_scene_to_file("res://scenes/levels/Floor1.tscn")
+
+func _on_submit_answer_pressed() -> void:
+	fraction_addition_checker(first_num, first_denum, second_num, second_denum)
+
+func _on_button_pressed() -> void:
+	return_to_world()
