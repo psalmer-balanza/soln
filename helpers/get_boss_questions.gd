355b9aba4e27626a3c29d41274c798dfec7b780d
Zen Lucas <2200501@slu.edu.ph>
2024-10-11 14:03:25 +0800

    add 2 global scripts (GetQuiz, GetFractions)
    GetQuiz -> get_boss_questions.gd
    GetFractions -> get_fraction_questions.gd

---
+++ helpers/get_boss_questions.gd
@@ -0,0 +1,57 @@
+extends Node
+
+# for boss battle scene
+var Enemy_HP = 100
+var Current_HP = 100
+var Question = false
+var tilemap: TileMapLayer
+var Question_Dictionary
+signal questions_loaded
+
+func _ready():
+	var getquestions_url = "http://localhost:3000/game/getmcquestions"
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._http_request_completed)
+
+	var post_data = {
+		"MinigameID": 5
+				}
+	var json_body = JSON.stringify(post_data)
+	var headers = ["Content-type: application/json"]
+	
+	# execute POST request
+	var error = http_request.request(getquestions_url, headers, HTTPClient.METHOD_POST, json_body)
+	if error != OK:
+		print("error: unable to make request")
+		
+# Called when the HTTP request is completed.
+func _http_request_completed(_result, response_code, _headers, body):
+	if response_code == 200:
+		var json = JSON.new()
+		var error = json.parse(body.get_string_from_utf8())
+		if error == OK:
+			var response = json.get_data()
+			Question_Dictionary = constructQuestionDictionary(response)
+			# Emit signal once questions are loaded
+			emit_signal("questions_loaded")
+	else:
+		print("HTTP request failed with code:", response_code)
+
+func constructQuestionDictionary(response):
+	Question_Dictionary = []
+	for i in range(response.size()):
+		var question = response[i]
+		Question_Dictionary += [[
+			question["question_text"], 
+			question["option_1"], 
+			question["option_2"], 
+			question["option_3"], 
+			question["option_4"], 
+			question["correct_answer"]
+			]]
+		
+	return Question_Dictionary
+
+# for smithing mini game
+var ores_inside:int = 0
