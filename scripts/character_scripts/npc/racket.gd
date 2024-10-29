4acb3ee1ec4f03ac36348a53731a681bb067e399
2205867 <153579536+2205867@users.noreply.github.com>
2024-10-29 22:36:43 +0800

    Fixed stupid raket gltich

--- scripts/character_scripts/npc/racket.gd
+++ scripts/character_scripts/npc/racket.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 
 func _process(_delta):
-	if DialogueState.current_quest == "find_raket_house" or DialogueState.current_quest == "starting":
+	if DialogueState.current_quest == "share_pie_with_raket" or DialogueState.current_quest == "starting":
 		manual_actionable(true)
 	if DialogueState.current_quest == "raket_house" or DialogueState.current_quest == "raket_house_worded_complete":
 		manual_actionable(false)
 	if DialogueState.current_quest == "raket_sword_quest":
 		manual_actionable(true)
-	# Raket sends you to face Snekkers
+	if DialogueState.current_quest == "face_the_snake":
+		$RadialDialogueIndicator.visible = false
 
 func manual_actionable(is_enabled: bool):
 	print(is_enabled)
