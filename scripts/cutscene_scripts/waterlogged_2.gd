f0bff1a97404e241e01ec12e9e0ca2c3a3fcab0f
2205867 <153579536+2205867@users.noreply.github.com>
2024-11-02 00:48:13 +0800

    Game over flow for waterlogged rooms

--- scripts/cutscene_scripts/waterlogged_2.gd
+++ scripts/cutscene_scripts/waterlogged_2.gd
@@ -7,3 +7,6 @@ func _ready() -> void:
 		$AutoActionable/CollisionShape2D.disabled = true
 		$AnimationPlayer.play("disappear")
 		$ExclamationMark.visible = false
+	elif not DialogueState.current_quest == "after_wr_2":
+		print("enabling auto in waterlogged 2")
+		$AutoActionable/CollisionShape2D.disabled = false
