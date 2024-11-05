c1700287af62ff9e09bdd108f3a5b806cd3888b3
MattTicag <153583264+MattTicag@users.noreply.github.com>
2024-11-05 19:45:27 +0800

    edited stuff

--- scenes/levels/test_floor1/firefly.gd
+++ scenes/levels/test_floor1/firefly.gd
@@ -1,1 +1,12 @@
-#firefly movement sucks
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("area entered")
+	animation_player.play("appear")
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	print("area exited")
+	animation_player.play("disappear")
