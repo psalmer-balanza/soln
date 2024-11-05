5af26e065c639a5f9138d19b7cdd61ddb79fdd22
MattTicag <153583264+MattTicag@users.noreply.github.com>
2024-11-05 21:18:07 +0800

    paused menu bugs

--- scenes/characters_scenes/floor1_characters/pause_screen.gd
+++ scenes/characters_scenes/floor1_characters/pause_screen.gd
@@ -2,24 +2,25 @@ extends Control
 
 @onready var objective = $"../ObjectiveOverlay"
 @onready var badges = $"../OnscreenBadges"
-@onready var character = $"../.."
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("pause"):
+		get_tree().paused = true
 		_pause()
 
 func _pause():
-	character.get_tree().paused = true
-	self.visible = true
+	show()
 	objective.visible = false
 	badges.visible = false
 
 func _on_back_pressed() -> void:
-	character.get_tree().paused = false
-	self.visible = false
+	print("unpaused")
+	get_tree().paused = false
+	hide()
 	objective.visible = true
 	badges.visible = true
 
 func _on_main_menu_pressed() -> void:
+	print("main menu")
 	#add game save here
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
