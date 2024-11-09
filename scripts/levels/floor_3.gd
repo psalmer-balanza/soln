7cfe21cade0602e09f3ddf13caa8f067b387b2b7
2205867 <153579536+2205867@users.noreply.github.com>
2024-11-09 20:52:59 +0800

    Floor3 autodialogue flow fix

---
+++ scripts/levels/floor_3.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if PlayerState.saved_scene == "res://scenes/levels/Floor3.tscn" and PlayerState.first_time_initializing_third_floor_scene == false:
+		var player_node = get_node("MainCharacter")
+		if player_node:
+			player_node.position = PlayerState.saved_position
+			print("Restored player position: ", PlayerState.saved_position)
+	else:
+		print("First time initializing Floor3, starting at spawn area")
+		Global.current_floor = 3
