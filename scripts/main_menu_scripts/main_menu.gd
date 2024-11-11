42ee1bd81dc368fb596ac5b94cac144252e1c328
2205867 <153579536+2205867@users.noreply.github.com>
2024-11-11 22:48:49 +0800

    deleted settings

--- scripts/main_menu_scripts/main_menu.gd
+++ scripts/main_menu_scripts/main_menu.gd
@@ -60,9 +60,6 @@ func _on_new_game_pressed() -> void:
 	Global.current_floor = 1
 	get_tree().change_scene_to_file("res://scenes/levels/Floor1.tscn")
 
-func _on_options_pressed() -> void:
-	pass # Replace with function body.
-
 func _on_quit_pressed() -> void:
 	get_tree().quit(0)
 
