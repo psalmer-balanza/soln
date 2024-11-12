6e44b1edb194362b745d42e946ac54d852f9ba1d
Julliard Macatuggal <2200905@slu.edu.ph>
2024-11-12 18:09:15 +0800

    Partial implement the snekkers mushroomed and healed scene, Fix bug for worded fraction where a question doesnt match the given fractions, Update question grammar

--- scripts/character_scripts/npc/snekkers.gd
+++ scripts/character_scripts/npc/snekkers.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var snekkers_healed = $SnekkersHealed
+@onready var snekkers_mushroomed = $SnekkersMushroomed
+
 
 func _process(delta):
 	# Collecting sword pieces or leaving floor 1
@@ -13,7 +16,15 @@ func _process(delta):
 		manual_actionable(true)
 
 	if DialogueState.current_quest == "you_may_leave_floor_1":
+		print("from snekkers, current quest ", DialogueState.current_quest)
+		snekkers_mushroomed.visible = false
+		snekkers_healed.visible = true
 		manual_actionable(true)
+	
+	if DialogueState.current_quest == "snake_quiz_complete":
+		print("from snekkers, current quest ", DialogueState.current_quest)
+		snekkers_mushroomed.visible = false
+		snekkers_healed.visible = true
 
 func manual_actionable(is_enabled: bool):
 	$Actionable/CollisionShape2D.disabled = !is_enabled
