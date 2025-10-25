f51df5df6c52ad5712011b0817f2143b323cd58b
Julliard Macatuggal <153582996+julliardmacatuggal@users.noreply.github.com>
2025-10-25 12:39:47 +0800

    Update copyright
    Adding a tooltip when hovering to the hexoffenders text. Then add Saint Louis University text below hexoff copyright.

---
+++ scripts/main_menu_scripts/copyright.gd
@@ -0,0 +1,19 @@
+extends RichTextLabel
+
+@onready var tooltip: Control = $"../Tooltip"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mouse_entered() -> void:
+	tooltip.visible = true
+
+func _on_mouse_exited() -> void:
+	tooltip.visible = false
